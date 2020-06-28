1aec4f2db960f88994a672644474236b7f792b13
motomotoyoshi <motomoto.git@gmail.com>
2020-06-29 02:08:47 +0900

    First commit

---
+++ lesson2/message.js
@@ -0,0 +1,7 @@
+"use strict";
+
+let messages = [
+  "A change of environment can be a good things!",
+  "You will make it!",
+  "Just run with the code!"
+];
---
+++ lesson2/positiveMessages.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let messages = [
+  "A change of environment can be a good things!",
+    "You will make it!",
+      "Just run with the code!"
+      ]
+messages.forEach(message => console.log(message));
