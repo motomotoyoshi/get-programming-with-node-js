0ab42710b78a653d80fbe75fc4078056f3ae7bd7
motomotoyoshi <motomoto.git@gmail.com>
2020-07-02 01:31:52 +0900

    update, complete lesson3

---
+++ lesson3/3-4/func.js
@@ -0,0 +1,3 @@
+"use strict";
+
+exports.addNum = (x, y) => x + y;
--- lesson3/recipe_connection/main.js
+++ lesson3/recipe_connection/main.js
@@ -1,7 +1,10 @@
 "use strict";
 
-const cities = require("cities");
+const addNum = require("../3-4/func");
+// const cities = require("cities");
 
-var myCity = cities.zip_lookup("10016");
+// var myCity = cities.zip_lookup("10016");
 
-console.log(myCity);
+// console.log(myCity);
+
+console.log(addNum.addNum(1, 4));
