8025afd242c77fbf278b6f5e3c06dbf2145a25f7
motomotoyoshi <motomoto.git@gmail.com>
2020-07-17 02:16:20 +0900

    update, lesson12-3

---
+++ lesson12/confetti_cuisine/controllers/homeController.js
@@ -0,0 +1,13 @@
+"use strict";
+
+exports.showCourses = (req, res) => {
+  res.render("courses");
+};
+
+exports.showSignUp = (req, res) => {
+  res.render("contact");
+};
+
+exports.postedSignUpForm = (req, res) => {
+  res.render("thanks");
+};
--- lesson12/confetti_cuisine/main.js
+++ lesson12/confetti_cuisine/main.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const app = express();
+const homeCOntroller = require("./controllers/homeController");
 
 app.set("port", process.env.PORT || 3000);
 app.use(
@@ -14,6 +15,9 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Welcome to Confetti Cuisine!");
 });
+app.get("/courses", homeCOntroller.showCourses);
+app.get("/contact", homeCOntroller.showSignUp);
+app.post("/contact", homeCOntroller.postedSignUpForm);
 
 app.listen(app.get("port"), () => {
   console.log(`http://localhost:${app.get("port")}`);
